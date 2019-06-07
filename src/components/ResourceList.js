bba880b471405aba4a54aa4abb57931cc43deb69
jerostax <jeremy.geneste@gmail.com>
2019-06-07 11:04:18 +0200

    [ REFACTO ] useEffect outsite of ResourceList fx component for code reuse

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = (resource) => {
   const [resources, setResources] = useState([]);
 
   useEffect(
@@ -14,7 +14,15 @@ const ResourceList = ({ resource }) => {
         setResources(response.data);
       })(resource);
     },
-    [resource])
+    [resource]
+  );
+
+  return resources;
+}
+
+const ResourceList = ({ resource }) => {
+  const resources = useResources(resource);
+
   return (
     <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
   );
